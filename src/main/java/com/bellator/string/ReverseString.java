a90d0fcf9a115db5930940cda2c7a7b7d97aeba7
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-18 19:20:28 +0530

    test failed

---
+++ src/main/java/com/bellator/string/ReverseString.java
@@ -0,0 +1,33 @@
+package com.bellator.string;
+
+public class ReverseString 
+{
+	// 1. using charAt(),
+	static void reverseString(String str)
+	{
+		String rev = "";
+		
+			for(int i=str.length()-1; i>=0; i--)
+			{
+				rev += str.charAt(i);
+			}
+		System.out.println("\n Original string given:"+str+"\n Reversed string:"+rev);
+	}
+	
+	// 2. using stringBuilder,
+	static void strReverse(String st)
+	{
+		var sb = new StringBuilder(st);
+		System.out.println("\n Original string given:"+st+"\n Reversed string:"+sb.reverse());	
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("\nProgram to reverse a string in Java\n");
+		System.out.println("\n1. Using charAt(),");
+		reverseString("Gradle");
+		System.out.println("\n2. Using StringBuilder");
+		strReverse("String");
+	}
+
+}
--- src/test/java/com/bellator/array/SumOfNumbersTest.java
+++ src/test/java/com/bellator/array/SumOfNumbersTest.java
@@ -2,13 +2,16 @@ package com.bellator.array;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 
 public class SumOfNumbersTest
 {
+	@Test
 	public void sumOfArrayTest()
 	{
 		var sumOfArr = new SumOfNumbers();
-		int actual = sumOfArr.sumOfArray(new int[]{1,2,3,4,5});
+		int actual = sumOfArr.sumOfArray(new int[]{1,2,3,4,5,6});
 		int expected = 15;
 		
 		assertEquals(expected, actual);
