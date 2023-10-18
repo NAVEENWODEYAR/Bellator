8de481dbf3061b0deca40d566e8191bf7a00a5b1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-18 20:02:03 +0530

    st.toLowerCase().matches(".*[aeiou].*");

--- src/main/java/com/bellator/string/SwappingNumbers.java
+++ src/main/java/com/bellator/string/SwappingNumbers.java
@@ -10,10 +10,21 @@ public class SwappingNumbers
 		a = a-b;	// 2
 		System.out.println("\n After swapping,a:"+a+",b:"+b);
 	}
+	
+	// Write a Java program to check if a vowel is present in a string
+	static boolean vowelIsPresent(String st)
+	{
+		boolean matches = st.toLowerCase().matches(".*[aeiou].*");
+		return matches;
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("\n Swapping two numbers without using third number.,");
 		swap2Numbers(1, 2);
+		System.out.println("*******");
+		boolean vowelIsPresent = vowelIsPresent("vowel");
+		System.out.println(vowelIsPresent);
 	}
 
 }
