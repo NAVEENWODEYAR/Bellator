7557b17d073b0bee21901a69307e7129757c52d6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-18 18:12:10 +0530

    initial commit

---
+++ src/main/java/com/bellator/BellatorApplication.java
@@ -0,0 +1,15 @@
+package com.bellator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellatorApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BellatorApplication.class, args);
+		System.out.println("\n Warrior");
+	}
+
+}
---
+++ src/test/java/com/bellator/BellatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
