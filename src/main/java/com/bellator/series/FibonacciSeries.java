94bed007b495ac56019f175d9665de8fbf81738a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-19 22:27:52 +0530

    return fiboNacciUsingRecursion(n-1)+fiboNacciUsingRecursion(n-2);

--- src/main/java/com/bellator/series/FibonacciSeries.java
+++ src/main/java/com/bellator/series/FibonacciSeries.java
@@ -2,6 +2,7 @@ package com.bellator.series;
 
 public class FibonacciSeries 
 {
+	// using for loop,
 	static void fiboNacciSeries(int n)
 	{
 		int a=0;
@@ -16,10 +17,21 @@ public class FibonacciSeries
 			c=a+b;
 		}
 	}
+	
+	// using recursion,.
+	static int fiboNacciUsingRecursion(int n)
+	{
+		if(n<= 1)
+			return n;
+		return fiboNacciUsingRecursion(n-1)+fiboNacciUsingRecursion(n-2);
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("\n******Fibonacci Series**********\n");
 		fiboNacciSeries(5);
+		System.out.println("********");
+		fiboNacciUsingRecursion(50);
 	}
 
 }
