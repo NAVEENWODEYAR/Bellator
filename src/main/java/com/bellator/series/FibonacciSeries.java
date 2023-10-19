cad7c76da4fb95866159d116e7d0b3557ee8ba1b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-19 22:23:33 +0530

    System.out.println(a+",");

--- src/main/java/com/bellator/series/FibonacciSeries.java
+++ src/main/java/com/bellator/series/FibonacciSeries.java
@@ -2,10 +2,24 @@ package com.bellator.series;
 
 public class FibonacciSeries 
 {
-
+	static void fiboNacciSeries(int n)
+	{
+		int a=0;
+		int b=1;
+		int c=1;
+		
+		for(int i=0; i<n; i++)
+		{
+			System.out.println(a+",");
+			a=b;
+			b=c;
+			c=a+b;
+		}
+	}
 	public static void main(String[] args) 
 	{
-
+		System.out.println("\n******Fibonacci Series**********\n");
+		fiboNacciSeries(5);
 	}
 
 }
