3e5e23c061882ed0925113d14d2ab54b6524610e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-19 20:56:37 +0530

    Prime Number program

--- src/main/java/com/bellator/string/PrimeNumber.java
+++ src/main/java/com/bellator/string/PrimeNumber.java
@@ -2,7 +2,19 @@ package com.bellator.string;
 
 public class PrimeNumber 
 {
-
+	static void primeNumber(int n)
+	{
+		System.out.println("\na prime number is a number that cannot be evenly divided by any other number except 1 and itself");
+		if(n==1 || n ==0)
+		{
+			System.out.println("");
+		}
+		if(n == 2)
+		{
+			System.out.println("");
+		}
+		
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("\n Prime Number program");
