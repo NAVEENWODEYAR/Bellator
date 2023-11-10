6e7ec6bcec7cca21dd0b38fb9a3b11d9e7d14cd9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-10 20:42:26 +0530

    @SpringBootTest

--- src/test/java/com/bellator/BellatorApplicationTests.java
+++ src/test/java/com/bellator/BellatorApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BellatorApplicationTests {
+class BellatorApplicationTests 
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		
 	}
 
 }
